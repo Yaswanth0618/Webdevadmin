f00d510827ed74d337f12705b1dd954d5644dd33
Yaswanth Kandra <yaswanthaditya.kandra@gmail.com>
2024-02-11 10:28:15 -0500

    updated

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-    localStorage.setItem('isLoggedIn', 'false');
-    function login() {
-      var username = document.getElementById('username').value;
-      var password = document.getElementById('password').value;
-      
-      if (username === 'CHadmin1289' && password === 'CHadminpass8912') {
-        alert('Login successful');
-        localStorage.setItem('isLoggedIn', 'true'); // Set login status to true
-        window.location.href = 'home.html'; // Redirect to main.html
-      } else {
-        alert('Invalid username or password');
-      }
-    }
+localStorage.setItem('isLoggedIn', 'false');
+function login() {
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  
+  if (username === 'CHadmin1289' && password === 'CHadminpass8912') {
+    alert('Login successful');
+    localStorage.setItem('isLoggedIn', 'true'); // Set login status to true
+    window.location.href = 'home.html'; // Redirect to main.html
+  } else {
+    alert('Invalid username or password');
+  }
+}
