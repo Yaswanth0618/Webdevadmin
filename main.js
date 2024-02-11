da0b22ac3dadcefc2f8997db1e2bb615d440bf79
Yaswanth Kandra <yaswanthaditya.kandra@gmail.com>
2024-02-11 11:21:52 -0500

    stored login in local storage

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-localStorage.setItem('isLoggedIn', 'false');
+src = "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js"
+
+localStorage.setItem("isLoggedIn", "true");
 function login() {
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  
-  if (username === 'CHadmin1289' && password === 'CHadminpass8912') {
-    alert('Login successful');
-    localStorage.setItem('isLoggedIn', 'true'); // Set login status to true
-    window.location.href = 'home.html'; // Redirect to main.html
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username === localStorage.getItem("username") && password === localStorage.getItem("password")) {
+    alert("Login successful");
+    localStorage.setItem("isLoggedIn", "true"); // Set login status to true
+    window.location.href = "home.html"; // Redirect to main.html
   } else {
-    alert('Invalid username or password');
+    alert("Invalid username or password");
   }
 }
+
+
