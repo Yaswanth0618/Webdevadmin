50012fad44cb3a6af3fc62dc620bdb8e62789a41
Yaswanth Kandra <yaswanthaditya.kandra@gmail.com>
2024-06-15 20:17:41 -0400

    updated login + better viewing + easy admin

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ function login() {
 */
 
 
+
+
+
+/**
+<body>
+  <div class="bodylogin">
+    <div class="containerxy">
+      <h2>Culinary Harbor Admin</h2>
+      <form id="loginForm">
+       <input type="text" id="username" placeholder="Username">
+        <input type="password" id="password" placeholder="Password">
+        <button type="submit" id="submit" >Login</button>
+     </form>
+    </div>
+  </div>
+
+</body>
+*/
